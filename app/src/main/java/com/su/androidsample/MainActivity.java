8f1dc7c64d0cba8bd4c3e158249467f81a58e1bc
Su Cong <sucong0826@users.noreply.github.com>
2017-07-27 20:37:07 +0800

    feat |> New feature about JNI practicing.
    1.I wrote the simplest sample about jni.

--- app/src/main/java/com/su/androidsample/MainActivity.java
+++ app/src/main/java/com/su/androidsample/MainActivity.java
@@ -2,6 +2,7 @@ package com.su.androidsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+
+    static {
+        System.loadLibrary("hello-jni");
     }
 }
